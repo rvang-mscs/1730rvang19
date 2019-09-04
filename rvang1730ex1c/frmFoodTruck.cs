e25c8b64394a8df7ca66eafee0094ed4a8a6514b
Rick Vang <qj3569fu@go.minnstate.edu>
2019-09-04 15:02:29 -0500

    Fixed some stuff

--- rvang1730ex1c/frmFoodTruck.cs
+++ rvang1730ex1c/frmFoodTruck.cs
@@ -23,32 +23,15 @@ namespace rvang1730ex1c
             txtHamburgersTotal.Text = (5.0m * Convert.ToDecimal(txtHamburgers.Text)).ToString("0.00");
             txtPreTax.Text = (Convert.ToDecimal(txtHotdogsTotal.Text) + Convert.ToDecimal(txtHamburgersTotal.Text)).ToString("0.00");
 
-            //Minnesota Tax
             double mnTax = 6.875 / 100;
 
-            //create double variable to convert value inside Pretax into double
             double tax = double.Parse(txtPreTax.Text);
 
-            //create double to temporary store the product of tax and mnTax
             double tax2 = tax* mnTax;
            
-            //convert temporary into string
             string taxString = Math.Round(tax2, 2).ToString();
-            
-            //txtTax = (Convert.ToDecimal(txtPreTax.Text) * ;
-            
-            //put back inside the box by converting string into whatever the box type is 
+
             txtTax.Text = taxString;
-
-            ////////////////////////////////////////////////////////////////////////////////////////////////////////
-            //add pretax total and tax then convert that name to double then use math.round to cut off 
-            //zeros then convert it back to string and put it inside textbox.
-            //round to 2 decimal place
-
-            //convert pretax total to double and convert tax to double then add then use math.round to get to 2 place deciaml
-            //then convert back to string and put string inside textbox but using txtTotal.Text.
-
-            //Math.Round( variable name to total of both, 2);
 
             double pretax = double.Parse(txtPreTax.Text);
             double Taxadd = double.Parse(txtTax.Text);
